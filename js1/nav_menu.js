a825f7295d909f8473efe2cf5ab58ab7bcccd473
DannyM1chael <coldsoulrusirk@gmail.com>
2019-09-11 11:01:25 +0300

    Organized

--- js1/nav_menu.js
+++
@@ -1,6 +0,0 @@
-function changeSize(className) {
-    var elements = document.getElementsByClassName(className);
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].style.size = Math.random();
-    }
-}
