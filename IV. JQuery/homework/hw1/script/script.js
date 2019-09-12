cf63a022d39e02aff79bc79fc73259501ec382fb
DannyM1chael <coldsoulrusirk@gmail.com>
2019-09-12 15:13:54 +0300

    12/09 15:00

---
+++ IV. JQuery/homework/hw1/script/script.js
@@ -0,0 +1,12 @@
+
+function changeSpan(){
+	$("span").css("color", "red").text("это спан")
+}
+
+function changeP(){
+	$("p").css("color", "green").text("это параграф")
+}
+
+function changeLi(){
+	$("li").css("color", "green").text("это элемент списка")
+}
