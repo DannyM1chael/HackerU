4766460273d9c57df38cbe1751655cc2d5f89bee
DannyM1chael <coldsoulrusirk@gmail.com>
2019-10-06 15:18:37 +0300

    06/10/2019

--- IV. JQuery/homework/hw5_6/script/script.js
+++ IV. JQuery/homework/hw5_6/script/script.js
@@ -1,20 +1,25 @@
 
 $(document).ready(function() {
 	$('#AJAXGetBtn').click(function(){
+		// забираем данные с input-data
 		var idVal = $('#input-data').val();
 		var idValStr = (idVal === '') ? '': `/${idVal}`;
+		// очищаем input-data после ввода
 		$('#input-data').val('');
-
+		// создаем объект для работы с функцией
 		var methods = {};
 		methods['success']      = successHandler;
 		methods['error']        = errorHandler;
 		methods['beforeSend']   = beforeSendHandler;
-
+		methods['complete']     = completeHandler;
+		// подключаем функцию, которая забирает данные и объект
 		AjaxGet(idValStr, methods);
 
-	})
+	});
 
-function singleDataHadler(data){
+// функция для создания элемента для данных - объекта
+
+function singleDataHandler(data){
 	var newDoHtml = [ 
 					`<div class='do-content'>`,
 						`<p>${data.id}</p>`,
@@ -26,13 +31,13 @@ function singleDataHadler(data){
 	$('.container').html(newDoHtml);
 
 }
-
-function dataHadler(data){
+// функция для создания элемента для данных - массива
+function dataHandler(data){
 	$('.container').html('');
 	data.forEach(function(element, index){
 		var newDoHtml = [ 
 						`<div class='do-content'>`,
-							`<span>${index}</span>`,
+							`<span>${index+1}</span>`,
 							`<h2>${element.title}</h2>`,
 							`<p>${element.completed}</p>`,
 						`</div>`
@@ -44,27 +49,33 @@ function dataHadler(data){
 
 }
 
+// проверяем полученные данные - объект или массив
 function successHandler(data, textStatus){
 	if (Array.isArray(data)){
 		dataHandler(data);
 	}else{
-		singleDataHadler(data);
+		singleDataHandler(data);
 	}
 
 }
 
+//функция при ошибке
 function errorHandler(data, textStatus){
 	alert('Такого id нет');				
 }
-
+// функция при загрузке
 function beforeSendHandler() {
 	$('.loadingWindow h1').show().animate({'width' : '100%'}, 500);
 	$('.loadingWindow h1').show().animate({'width' : '0%'}, 500);
 
 }
+// функция при выполнении запроса
+function completeHandler(){
+	console.log('Done');
+}
 
 
-
+// создаем функцию, которая забирает данные и объект
 function AjaxGet(idValStr, methods){
 	var AjaxGetObj         = {};
 	AjaxGetObj['url']      = `https://jsonplaceholder.typicode.com/todos/${idValStr}`;
