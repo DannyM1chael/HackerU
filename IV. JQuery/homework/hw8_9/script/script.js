c25647679901999a134b771d2d26cc459e7f29c2
DannyM1chael <coldsoulrusirk@gmail.com>
2019-10-11 23:29:13 +0300

    11/10/2019

---
+++ IV. JQuery/homework/hw8_9/script/script.js
@@ -0,0 +1,27 @@
+
+	var nav  = $('nav');
+	var menu = $('nav h1');
+	var main = $('main');
+	open = false;
+	hover = false;
+
+menu.on('click', function(){
+	openmenu = !open ? true: false;
+	nav.toggleClass('menu-active');
+	main.toggleClass('menu-active');
+	nav.removeClass('menu-hover');
+	main.removeClass('menu-hover');
+});
+
+menu.hover(function(){
+	if(!open){
+		nav.addClass('menu-hover');
+		main.addClass('menu-hover');
+	}
+})else(function(){
+		nav.removeClass('menu-hover');
+		main.removeClass('menu-hover');
+	}
+)
+
+
