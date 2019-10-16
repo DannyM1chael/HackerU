10b08842d85f6d4de73fe6e6d207fc772101c601
DannyM1chael <coldsoulrusirk@gmail.com>
2019-10-16 14:20:41 +0300

    16/10/2019

--- IV. JQuery/homework/hw8_9/script/script.js
+++ IV. JQuery/homework/hw8_9/script/script.js
@@ -4,19 +4,16 @@ $(document).ready(function() {
 
 	burgerMenu.on('click', function(){
 		navMenu.toggleClass('links-active');
-
-	})
-
-	burgerMenu.find('div .line1').on('click',function(){
-		$(this).toggleClass('toggle');
-
-	})
+		burgerMenu.find('.line1').toggleClass('toggle1');
+		burgerMenu.find('.line2').toggleClass('toggle2');
+		burgerMenu.find('.line3').toggleClass('toggle3');
+	});
 
 	navMenu.find('li').on('click', function(){
 		scrollHandler($(this));
-	})
+	});
 
 	function scrollHandler(obj){
-		$('html').animate({scrollTop:$('.page4').eq(obj.index()).offset().top})};
+		$('html').animate({scrollTop:$(`section`).eq(obj.index()).offset().top})};
 
 })
