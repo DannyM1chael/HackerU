1ed90a75293897d382aeeb4ec6acb272fb18d786
DannyM1chael <coldsoulrusirk@gmail.com>
2019-10-21 13:55:32 +0300

    21/10/2019

--- I. HTML_CSS/Restaraunt/script/script.js
+++ I. HTML_CSS/Restaraunt/script/script.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
 	var burgerMenu = $('.burger');
+	var nav = $('.navigation');
 	var innerMenu = $('.burger-inner');
 
 	burgerMenu.on('click', function(){
+		innerMenu.toggleClass('active');
 		burgerMenu.find('.line1').toggleClass('toggle1');
 		burgerMenu.find('.line2').toggleClass('toggle2');
 		burgerMenu.find('.line3').toggleClass('toggle3');
