43f896924e739925b8282008ba642008599e12a7
DannyM1chael <coldsoulrusirk@gmail.com>
2019-11-02 13:57:04 +0300

    02/11/2019

---
+++ VI. React with Redux/classwork/batman/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MovieCard from './components/moviecard'
+
+function App() {
+  return (
+    <div class="container">
+      <MovieCard />
+    </div>
+  );
+}
+
+export default App;
