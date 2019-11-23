7d4dd9477966c3dd14ead734d5363a3b6c89a2bd
DannyM1chael <coldsoulrusirk@gmail.com>
2019-11-23 20:59:37 +0300

    23/11/2019

--- IV. JQuery/homework/hw10/script/script.js
+++ IV. JQuery/homework/hw10/script/script.js
@@ -1,15 +1,43 @@
 $(document).ready(function(){
-    $('#AJAXbtn').click(function(){
+    $('#AJAXGetbtn').click(function(){
+        $(this).hide(500);
         $.ajax({
             url: 'https://reqres.in/api/users',
             dataType: 'json',
             success: function(data, textStatus){
-                console.log(textStatus)
-                console.log(data)
+                dataHandler(data);
             },
             error: function(){
-                console.log('Bad news:(')
+                console.log('Ошибка')
             }
-        })
-    })
+        });
+    });
+
+    function dataHandler(data) {
+        console.log('вывод существующих пользователей');
+    };
+
+    $('#AJAXPostbtn').click(function(){
+        $.ajax({
+            type: 'POST',
+            url: 'https://reqres.in/api/users',
+            data: JSON.stringify({
+                'email': 'email',
+                'first_name': 'name',
+                'last_name': 'surname',
+                'avatar': 'avatar'
+            }),
+            success: function(response){
+                console.log(response);
+            },
+            error: function(){
+                console.log('Ошибка')
+            }
+        });
+    });
+
+    function dataHandlerUser(data) {
+        console.log('вывод новых пользователей');
+    };
+
 })
