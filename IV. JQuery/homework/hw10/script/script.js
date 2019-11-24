db6ac69fe8a75d6c580bc8a531789158da7caf4b
DannyM1chael <coldsoulrusirk@gmail.com>
2019-11-24 14:08:35 +0300

    24/11/2019

--- IV. JQuery/homework/hw10/script/script.js
+++ IV. JQuery/homework/hw10/script/script.js
@@ -5,75 +5,91 @@ $(document).ready(function(){
             url: 'https://reqres.in/api/users',
             dataType: 'json',
             success: function(data, textStatus){
-                console.log(data);
-                dataHandler(data);
+                dataHandler(data)
             },
             error: function(){
-                console.log('There is mistake')
+                console.log('There is a mistake');
             }
         });
     });
 
     function dataHandler(data) {
-        var existUser = [
-            '<div>',
-                `<img>${data.avatar}`,
-                `<p>${data.first_name}</p>`,
-                `<p>${data.last_name}</p>`,
-                '<div id="AJAXChooseBtn">CHOOSE TEAM MEMBER',
-                '</div>',
-                '<div id="AJAXDeleteBtn">DELETE TEAM MEMBER',
+        $.each(data, function(key, i){
+            if (Array.isArray(i)) {
+                $.each(i, function(index, j){
+                    var existUser = [
+                        '<div class="profile">',
+                            `<p>${j.first_name}</p>`,
+                            `<p>${j.last_name}</p>`,
+                            '<div id="AJAXChooseBtn">CHOOSE TEAM MEMBER',
+                            '</div>',
+                            '<div id="AJAXDeleteBtn">DELETE TEAM MEMBER',
+                            '</div>',
+                        '</div>'
+                    ].join('');
+                    $('.container').html(existUser);
+                    $('#AJAXChooseBtn').click(function(){
+                        $(this).parent().css('background-color', '#0be881');
+                        $(this).hide();
+                    });
+                    $('#AJAXDeleteBtn').click(function(){
+                        $.ajax({
+                            type: 'DELETE',
+                            url: 'https://reqres.in/api/users',
+                            success: function(response){
+                                dataHandlerUser(response);
+                            },
+                            error: function(){
+                                console.log('There is mistake')
+                            }
+                        });
+                    });
+                    function dataHandlerUser(response) {
+                        console.log('удаление пользователя');
+                    };
+                })
+            };
+        });
+        var addUser = [
+            '<div class="profile">',
+                '<div id="AJAXPostBtn">ADD NEW TEAM MEMBER',
                 '</div>',
             '</div>'
         ].join('');
-        var newUser = [
-            '<div id="AJAXPostBtn">ADD NEW TEAM MEMBER',
-            '</div>',
-        ].join('');
-        $('.container').append(existUser);
-        $('.container').append(newUser);
-    };
-
-    $('#AJAXPostBtn').click(function(){
-        $.ajax({
-            type: 'POST',
-            url: 'https://reqres.in/api/users',
-            data: JSON.stringify({
-                'email': 'email',
-                'first_name': 'name',
-                'last_name': 'surname',
-                'avatar': 'avatar'
-            }),
-            success: function(response){
-                dataHandlerUser(response);
-            },
-            error: function(){
-                console.log('There is mistake')
-            }
+        $('.container').append(addUser);
+        $('#AJAXPostBtn').click(function(){
+            $.ajax({
+                type: 'POST',
+                url: 'https://reqres.in/api/users',
+                data: JSON.stringify({
+                    'email': 'email',
+                    'first_name': 'name',
+                    'last_name': 'surname',
+                    'avatar': 'avatar'
+                }),
+                success: function(response){
+                    dataHandlerUser(response);
+                },
+                error: function(){
+                    console.log('There is a mistake');
+                }
+            });
         });
-    });
-
-    function dataHandlerUser(response) {
-        console.log('вывод новых пользователей');
+    
+        function dataHandlerUser(response) {
+            var addInfo = [
+                '<div class="profile">',
+                    '<input placeholder="first name"></input>',
+                    '<input placeholder="last name"></input>',
+                    '<input placeholder="email"></input>',
+                    '<div id="AJAXChooseBtn">CHOOSE TEAM MEMBER',
+                    '</div>',
+                    '<div id="AJAXDeleteBtn">DELETE TEAM MEMBER',
+                    '</div>',
+                '</div>'
+            ].join('');
+            $('.container').append(addInfo);
+        };
     };
-
-    $('#AJAXDeleteBtn').click(function(){
-        $.ajax({
-            type: 'DELETE',
-            url: 'https://reqres.in/api/users',
-            success: function(response){
-                dataHandlerUser(response);
-            },
-            error: function(){
-                console.log('There is mistake')
-            }
-        });
-    });
-
-    function dataHandlerUser(response) {
-        console.log('удаление новых пользователей');
-    };
-
-
 
 })
