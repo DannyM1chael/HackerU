2838daec4616a7cc479c3bd0995532baa3c896ae
DannyM1chael <coldsoulrusirk@gmail.com>
2019-11-30 19:18:07 +0300

    30/11/2019

--- VII. Frontend/project/ecommerce/src/App.js
+++ VII. Frontend/project/ecommerce/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Footer from '../src/components/footer';
 import AdBottom from '../src/components/ad_bottom';
+import Partners from '../src/components/partners';
 
 function App() {
   return (
     <React.Fragment>
+      <Partners />
       <AdBottom />
       <Footer />
     </React.Fragment>
