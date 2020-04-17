d04fbc4fbbad415ae834ede88f699b78bacd220d
DannyM1chael <coldsoulrusirk@gmail.com>
2020-04-17 16:10:30 +0300

    17/04/2020

--- 11_PHP_Advanced/practice/func.php
+++ 11_PHP_Advanced/practice/func.php
@@ -1,21 +1,65 @@
 <?php
 
-define('SITE_URL',"localhost");
+class Treeview
+{
+    private $root;
 
-function listFolderFiles($dir){
-   $fileFolderList = scandir($dir);
-   echo '<ul>';
-   foreach($fileFolderList as $fileFolder){
-       if($fileFolder != '.' && $fileFolder != '..'){
-           if(!is_dir($dir.'/'.$fileFolder)){
-               echo '<li><a target="_blank" href="'.SITE_URL.'/'.ltrim($dir.'/'.$fileFolder,'./').'">'.$fileFolder.'</a>';
-           } else {
-               echo '<li>'.$fileFolder;
-           }
-           if(is_dir($dir.'/'.$fileFolder)) listFolderFiles($dir.'/'.$fileFolder);
-               echo '</li>';
-           }
-   }
-   echo '</ul>';
+    public function __construct($path)
+    {
+        $this->root = $path;
+    }
+
+    public function buildTree()
+    {
+        return $this->createStructure($this->root, true);
+    }
+
+    private function createStructure($directory, $root)
+    {
+        $structure = $root ? '<ul class = "treeview">' : '<ul>';
+
+        $nodes = $this->getNodes($directory);
+        foreach($nodes as $node){
+            $path = $directory . '/' . $node;
+            if(is_dir($path)){
+                $structure .= '<li class="treeview-folder">';
+                $structure .= '<span>' . $node . '</span>';
+                $structure .= self::createStructure($path, false);
+                $structure .= '</li>';
+            } else {
+                $path = str_replace($this->root . '/', null, $path);
+                $structure .= '<li class="treeview-file">';
+                $structure .= '<a href="#?' . $path . '">' . $node . '</a>';
+                $structure .= '</li>';
+            }
+        }
+        return $structure . '</ul>';
+    }
+
+    private function getNodes($directory = null)
+    {
+        $folders = [];
+        $files = [];
+
+        $nodes = scandir($directory);
+        foreach($nodes as $node){
+            if(!$this->exclude($node)){
+                if(is_dir($directory . '/' . $node)){
+                    $folders[] = $node;
+                } else {
+                    $files[] = $node;
+                }
+            }
+        }
+
+        return array_merge($folders, $files);
+    }
+
+    private function exclude($filename)
+    {
+        return in_array($filename, ['.', '..', 'index.php', '.htaccess', '.DS_Store']);
+    }
 }
-listFolderFiles('uploads/');
+
+$treeview = new Treeview('practice');
+echo $treeview->buildTree();
